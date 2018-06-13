e4d0767e5305298dad7173058cf50b488a8935d2
thanhnh <thanhnh1990@gmail.com>
2018-06-13 19:32:27 +0700

    update

--- tumblr/tumblr/Hub.cs
+++ tumblr/tumblr/Hub.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace tumblr
 {
     class Hub
     {
-        public string Img(string boardname, string note, string image, string link, int type)
+        public string Photo_Posts(string boardname, string note, string image, string link, int type)
         {
             boardname = boardname.Replace(" ", "-");
-            string url = "https://api.pinterest.com/v1/pins/?access_token=Ab1rhweqeChC5tmKgfILFrE6t-TYFNwv7w_soZhELMu2AwBCWQAAAAA&fields=id%2Clink%2Cnote%2Curl";
+            string url = "";
 
             StringBuilder sb = new StringBuilder();
             if (!string.IsNullOrEmpty(boardname))
@@ -28,7 +29,7 @@ namespace tumblr
                 sb.Append("&image_url=" + HttpUtility.UrlEncode("https:" + image.Replace("https:", "")));
 
             string data = sb.ToString().Substring(1);
-            CallApi call = new CallApi();
+            Call call = new Call();
             return call.Post(url, data);
         }
     }
